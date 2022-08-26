4c23a81ce812910e9b182517fc50fc22b8a9c034
pandada <viej0102@gmail.com>
2022-08-26 20:41:17 +0900

    애플코딩 JS입문 2-13강까지 완료

--- tab.js
+++ tab.js
@@ -27,11 +27,24 @@
 //   $('.tab-content').eq(2).addClass('show')
 // })
 
-for (let i = 0; i < $('.tab-button').length; i++){
-  $('.tab-button').eq(i).on('click', function(){
-    $('.tab-button').removeClass('orange')
-    $('.tab-button').eq(i).addClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-content').eq(i).addClass('show')
-  })
+
+
+// 탭 for문으로 만들기
+// for (let i = 0; i < $('.tab-button').length; i++){
+//   $('.tab-button').eq(i).on('click', function(){
+//     탭열기(i)
+//   })
+
+
+// 이벤트리스너 1개만 써서 탭 만들기
+$('.list').click(function(e){
+  탭열기(e.target.dataset.id)
+})
+
+
+function 탭열기(i) {
+  $('.tab-button').removeClass('orange')
+  $('.tab-button').eq(i).addClass('orange')
+  $('.tab-content').removeClass('show')
+  $('.tab-content').eq(i).addClass('show')
 }
